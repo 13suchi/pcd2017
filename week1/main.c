42b393bb928fc495f961811da4df1a807763ccfa
13suchi <suim16cs@cmrit.ac.in>
2017-02-08 11:08:36 +0530

    Update main.c

--- week1/main.c
+++ week1/main.c
@@ -17,3 +17,19 @@ int main()
     printf("result is:%d",d);
     return 0;
 }
+
+
+#include <stdio.h>
+
+int add(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+ int a=5;
+ int b=10;
+ int c=add(a,b);
+ printf("result is:%d",c);
+ return 0;
+}    
