bb2057e05919d5890f1ce69f178e0bcd53ee26e8
13suchi <suim16cs@cmrit.ac.in>
2017-02-13 15:44:36 +0530

    Update main.c

--- week1/main.c
+++ week1/main.c
@@ -32,4 +32,28 @@ int main()
  int c=add(a,b);
  printf("result is:%d",c);
  return 0;
-}    
+} 
+sum of 2 complex no's.
+#include<stdio.h>
+typedef struct
+{
+    float real;
+    float imag;
+}complex;
+ complex add(complex r1,complex r2)
+{
+    complex res;
+    res.real=r1.real+r2.real;
+    res.imag=r1.imag+r2.imag;
+    return res;
+}
+ int main()
+{
+    complex r1={4,6};
+    complex r2={3,8};
+    complex r3=add(r1,r2);
+    printf("result:%f+i.%f\n",r3.real,r3.imag);
+    return (0);
+}
+          
+    
